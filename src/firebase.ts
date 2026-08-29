9805254aa0eb20830d27731e6f376e1f5a814bb1
Annu Dhaneja <andad622@gmail.com>
2026-08-29 20:56:36 +0530

    Import enableNetwork from firebase/firestore
    Added enableNetwork import to manage Firestore network settings.

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,8 +6,7 @@ import {
   signOut,
   User,
 } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
+import { getFirestore, enableNetwork } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
